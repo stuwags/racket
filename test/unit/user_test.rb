399d4df99b0a7617ddd3dd9741a99f0349ab5c8a
Stuart Wagner <stuwags@gmail.com>
2012-11-17 14:50:46 -0500

    Added annotate

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id           :integer          not null, primary key
+#  first_name   :string(255)
+#  last_name    :string(255)
+#  photo        :string(255)
+#  access_token :string(255)
+#  facebook_id  :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
