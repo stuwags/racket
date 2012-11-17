baf105bd80a3fef0bfd087add8dd79d5950fb65c
Stuart Wagner <stuwags@gmail.com>
2012-11-17 15:23:48 -0500

    Adding User DB backend to data from singly

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,3 @@
-require "httparty"
-
 class AuthController < ApplicationController
 
   SINGLY_API_BASE = "https://api.singly.com"
@@ -7,7 +5,27 @@ class AuthController < ApplicationController
   def callback
     auth = request.env["omniauth.auth"]
     session[:access_token] = auth.credentials.token
+    @token = session[:access_token]
     redirect_to :root
+
+    @user = User.new
+
+    # @user = session[user]
+    # @user.first_name = session[first_name]
+    # @user.last_name = session[last_name]
+    # @user.phot0 = session[photo]
+    # @user.access_token = session[access_token]
+    # @user.facebook_id = session[facebook_id]
+    # if @user.save
+    #   redirect_to :root
+    # else
+    #   render :callback
+    # end
+  end
+
+  def show
+    @response = HTTParty.get("https://api.singly.com/profiles/facebook?access_token=#{session[:access_token]}")
+
   end
 
   def logout
