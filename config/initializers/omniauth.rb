d348b5dc2b2ae310b7812957bace37844bb7fb20
Stuart Wagner <stuwags@gmail.com>
2012-11-17 14:45:14 -0500

    Added Auth for Singly

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :singly, ENV['SINGLY_ID'], ENV['SINGLY_SECRET']
+  provider :singly, "73a0faf7277f8c4c03c4d62b30d5f536", "639402f8b00ad3242993e73437e6a79a"
 end
